24b6f516947170b09009ac6ac6303794c8ad965a
Eddie Michael <eddiemichael1983@gmail.com>
2020-09-23 20:16:26 -0700

    add initial HTML, CSS, and JS code

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#poster-showing").toggle();
+    $("#poster-hidden").toggle();
+  });
+});
+ 
+$(document).ready(function() {
+  $("img").click(function() {
+      alert("ARNOLD SCHWARZENEGGER AND CARL WEATHERS IN PREDATOR");
+      alert("GET TO THE CHOPPER!!!");
+    });
+  });
+  
